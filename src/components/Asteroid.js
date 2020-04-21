7ff011928b3dd505864cf7fd5020cee964c1dad6
mukeshjha <mukeshjha2007@gmail.com>
2020-04-22 02:46:39 +0530

    final assigment

---
+++ src/components/Asteroid.js
@@ -0,0 +1,115 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  Image,
+  TextInput,
+  Dimensions,
+  FlatList,
+  TouchableHighlight,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+const {height, width} = Dimensions.get('window');
+//https://restcountries.eu/rest/v2/name/india
+const apiKey="vQ438WTF06U3qAjryMGqp3M286Wgqg5IcWwLyYXT"
+export default class Asteroid extends React.Component {
+  state = {
+    data: {}
+  };
+
+  componentDidMount = async () => {
+    const {asteroid} = this.props.route.params;
+    console.log(this.props.route.params)
+    this.setState({asteroid: asteroid});
+    let response = await this.fetchAsteroid(asteroid);
+    this.setState({data: response});
+    console.log(response)
+    // if ('message' in repsonse) {
+    //   this.setState({error: repsonse.message});
+    // } else {
+    //   this.setState({asteroid: repsonse});
+    // }
+
+    //console.log(repsonse.status);
+  };
+
+  fetchAsteroid = async (asteroid) => {
+    const response = await fetch(`https://api.nasa.gov/neo/rest/v1/neo/${asteroid}?api_key=${apiKey}`, {});
+    const json = await response.json();
+    return json;
+  };
+ 
+
+  render() {
+    const {asteroid, data} = this.state;
+    return (
+      <>
+        <StatusBar barStyle="dark-content" />
+        <SafeAreaView>
+         <View style={styles.container}>
+              <View style={styles.itemContainer}>
+                <Text style={styles.title}>Asteroid ID :   <Text style={styles.text}> {asteroid}</Text></Text>
+                <Text style={styles.title}>Name :   <Text style={styles.text}> {data.name}</Text></Text>
+                <Text style={styles.title}>Nasa Jpl Url  :   <Text style={styles.url}> {data.nasa_jpl_url}</Text></Text>
+                <Text style={styles.title}>Is Potentially Hazardous Asteroid  :   <Text style={styles.text}> {data.is_potentially_hazardous_asteroid?"YES":"NO"}</Text></Text>
+                      
+            </View>
+         </View>
+          
+        </SafeAreaView>
+      </>
+    );
+  }
+}
+
+// Styles
+const styles = StyleSheet.create({
+  container: {
+    height: height,
+    width: width,
+  },
+
+  itemContainer: {
+    padding: 10,
+    margin: 10,
+    display: 'flex',
+    justifyContent: 'space-between',
+    flexDirection: 'column',
+    fontSize: 22,
+    fontWeight: 'bold',
+    elevation:1,
+    backgroundColor: 'white',
+    textAlign: 'center',
+    color: 'red',
+    borderRadius: 10,
+    borderColor: 'red',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.5,
+    shadowRadius: 2,
+    elevation: 2,
+  },
+  text: {
+    fontFamily: 'System',
+    fontSize: 17,
+    fontWeight: '400',
+    color: '#000',
+  },
+  title: {
+    fontFamily: 'System',
+    fontWeight: 'bold',
+    fontSize: 15,
+    fontWeight: '700',
+    color: '#000',
+  },
+  url: {
+    fontFamily: 'System',
+    fontWeight: 'bold',
+    fontSize: 14,
+    fontWeight: '700',
+    color: 'blue',
+  }
+ 
+});
---
+++ src/components/Home.js
@@ -0,0 +1,127 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  TextInput,
+  Button,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+//Api vQ438WTF06U3qAjryMGqp3M286Wgqg5IcWwLyYXT
+//https://api.nasa.gov/planetary/apod?api_key=vQ438WTF06U3qAjryMGqp3M286Wgqg5IcWwLyYXT
+
+export default class Home extends React.Component {
+  state = {
+    text: '',
+  };
+  onChangeText = (text) => {
+    this.setState({text});
+  };
+
+
+  
+  getRandomAsteroid =async()=>{
+    const {navigate} = this.props.navigation;
+    try{
+      let response= await this.fetchRandomAsteroid()
+      let id=response.near_earth_objects[this.getRandomInt(0,response.near_earth_objects.length)].id
+      navigate('Asteroid', {asteroid: id})
+    }
+      catch(e){
+        console.log(e)
+    }   
+  }
+
+
+  fetchRandomAsteroid = async () => {
+    const response = await fetch(
+      `https://api.nasa.gov/neo/rest/v1/neo/browse?api_key=DEMO_KEY`,
+      {},
+    );
+    const json = await response.json();
+    return json;
+  };
+
+
+  getRandomInt=(min, max)=> {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
+  render() {
+    const {navigate} = this.props.navigation;
+
+    return (
+      <>
+        <StatusBar barStyle="dark-content" />
+        <SafeAreaView>
+          <View>
+            <TextInput
+              keyboardType='numeric'
+              placeholder={'Enter Asteroid ID'}
+              style={styles.textInput}
+              numberOfLines={4}
+              onChangeText={(text) => this.onChangeText(text)}
+              value={this.state.text}
+            />
+            <View style={styles.btnview}>
+              <View style={styles.button} >
+                  <Button
+                    onPress={()=>this.getRandomAsteroid()}
+                    title="Random Asteroid"
+                  />
+                </View>
+                <View style={styles.button} >
+                  <Button
+                    disabled={!this.state.text.length > 0}
+                    onPress={() => navigate('Asteroid', {asteroid: this.state.text})}
+                    title="Submit"
+                  />
+                </View>
+              
+            </View>
+            
+              
+           
+          </View>
+        </SafeAreaView>
+      </>
+    );
+  }
+}
+const styles = StyleSheet.create({
+  btnview:{
+    display: 'flex',
+    flexDirection:'column-reverse',
+    marginLeft:'20%',
+    marginRight:'20%',
+    marginTop:'4%',
+    justifyContent:'space-between'
+
+  },
+  button: {
+    backgroundColor:'orange',
+    borderRadius:10,
+    alignSelf:'center',
+    color:'#000',
+    width:200,
+    marginTop:10, 
+ },
+  textInput: {
+    marginTop: 50,
+    alignSelf: 'center',
+    width: 200,
+    textAlign: 'center',
+    height: 50,
+    borderRadius: 10,
+    //elevation: 5,
+    backgroundColor: 'white',
+    shadowColor: 'white',
+    shadowRadius: 5,
+    shadowOpacity: 1,
+  },
+});
